ee4bccf4fffe1290bf3f08beaf9585c7f1b3a55a
ia23b-elbendarin <145666597+ia23b-elbendarin@users.noreply.github.com>
2025-06-03 12:01:18 +0200

    the final files

---
+++ m295_Nour/src/main/java/org/example/m295_nour/M295NourApplication.java
@@ -0,0 +1,13 @@
+package org.example.m295_nour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M295NourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M295NourApplication.class, args);
+    }
+
+}
---
+++ m295_Nour/src/test/java/org/example/m295_nour/M295NourApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.m295_nour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M295NourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
